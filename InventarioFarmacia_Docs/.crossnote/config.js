a94777c3d528d132ec078734d8232cf55108e2ff
Bryan <bryangod194@gmail.com>
2025-08-08 23:40:03 -0400

    se agrego documentacion estilizada

---
+++ InventarioFarmacia_Docs/.crossnote/config.js
@@ -0,0 +1,20 @@
+({
+  katexConfig: {
+    macros: {},
+  },
+
+  mathjaxConfig: {
+    tex: {},
+    options: {},
+    loader: {},
+  },
+
+  mermaidConfig: {
+    startOnLoad: false,
+  },
+  previewConfig: {
+    theme: "dark",
+    codeBlockTheme: "atom-one-dark",
+    showLineNumbers: true,
+  },
+});
