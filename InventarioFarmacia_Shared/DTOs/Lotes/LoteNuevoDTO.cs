9187518ffa187014dff945a53a1eb237bb336c44
Bryan <bryangod194@gmail.com>
2025-09-06 07:13:38 -0400

    adicion de metodos y componentes para los lotes, falta más conexión entre childrens y componente padre

--- InventarioFarmacia_Shared/DTOs/Lotes/LoteNuevoDTO.cs
+++ InventarioFarmacia_Shared/DTOs/Lotes/LoteNuevoDTO.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using InventarioFarmacia_Domain.Models;
 using InventarioFarmacia_Domain.Constants;
+using System.ComponentModel.DataAnnotations;
 
 namespace InventarioFarmacia_Shared.DTOs.Lotes
 {
     public class LoteNuevoDTO
     {
-        public DateOnly Fecha_Vencimiento { get; set; }
+        [Required(ErrorMessage = "La fecha de vencimiento es obligatoria")]
+        public DateOnly Fecha_Vencimiento { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        [Required(ErrorMessage = "El Nro de Lote es obligatorio")]
         public string Nro_Lote { get; set; } = null!;
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad de productos debe ser al menos 1")]
         public int Cantidad_Productos { get; set; }
         public LoteNuevoDTO() { }
     }
